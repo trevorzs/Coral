555c85a660f738751c2a783ec0509c4e859e470b
trevorzs <tzs2@cornell.edu>
2018-12-01 22:42:38 -0500

    resize fix

--- sketch4.js
+++ sketch4.js
@@ -23,6 +23,9 @@ rules[2] = {
   b: "→F[+F]F[-F][F]"
 }
 
+let swidth = Math.min(screen.width,window.innerWidth);
+let sheight = Math.min(screen.height,window.innerHeight);
+
 
 
 function setup() {
@@ -34,30 +37,32 @@ function setup() {
     bubbles.push(new Bubble());
   }
   for (var i = 1; i < 8; i++) {
-    coral.push(new Coral(window.visualViewport.width/3+i*window.visualViewport.width/25,window.visualViewport.height-100));
+    coral.push(new Coral(swidth/3+i*swidth/23,sheight-100));
   }
 }
 
 function generate(){
   coral = [];
   for (var i = 1; i < 8; i++) {
-    coral.push(new Coral(window.visualViewport.width/3+i*window.visualViewport.width/25,window.visualViewport.height-100));
+    coral.push(new Coral(swidth/3+i*swidth/23,sheight-100));
   }
 }
 
 function windowResized(){
+  swidth = Math.min(screen.width,window.innerWidth);
+  sheight = Math.min(screen.height,window.innerHeight);
   for (var i = 0; i < coral.length; i++) {
-    coral[i].x = window.visualViewport.width/3+i*window.visualViewport.width/25 + window.visualViewport.width/21;
-    coral[i].y = window.visualViewport.height-100;
+    coral[i].x = swidth/3+i*swidth/23 + swidth/21;
+    coral[i].y = sheight-100;
   }
 }
 
 function draw() {
   background(50, 89, 100);
-  button.position(window.visualViewport.width-200,50);
+  button.position(swidth-200,50);
   noStroke();
   fill(156, 118, 73);
-  rect(0,window.visualViewport.height-100,window.visualViewport.width,100);
+  rect(0,sheight-100,swidth,100);
   stroke(255);
   noFill();
   for (var i = 0; i < bubbles.length; i++) {
@@ -73,7 +78,7 @@ function draw() {
   translate(-75,-100);
   textSize(36);
   textFont('Monoton');
-  text("coral",window.visualViewport.width/2,window.visualViewport.height/2);
+  text("coral",swidth/2,sheight/2);
   pop();
   for (var i = 1; i < coral.length; i+= 2) {
     coral[i].render();
